e7d1dcce57ef42ebf54e53ef40f8b9fdc67947a8
Joaquim Gomes <joaquim.gomes@frotcom.com>
2023-03-22 18:42:22 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by joaquim de oliveira gomes on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
